b1a200a0645f3acc19cb29f48c6000928abfd112
Ant Somers <antsomers@gmail.com>
2021-03-08 02:13:07 +0300

    Fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,15 @@
-const {i18n} = require('./next-i18next.config');
+const { i18n } = require("./next-i18next.config");
 
 module.exports = {
   i18n,
   async rewrites() {
-    return [
-      {source: '/tr/hakkimizda', destination: '/about', locale: false},
-    ];
+    return [{ source: "/tr/hakkimizda", destination: "/about", locale: false }];
   },
   async redirects() {
     return [
       {
-        source: '/tr/about',
-        destination: '/tr/hakkimizda',
+        source: "/tr/about",
+        destination: "/tr/hakkimizda",
         locale: false,
         permanent: true,
       },
