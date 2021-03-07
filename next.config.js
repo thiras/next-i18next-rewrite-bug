e63ea503c96b2447a2438a1a81b98a95b6505f7a
Ant Somers <antsomers@gmail.com>
2021-03-08 02:42:25 +0300

    Add denemesayfasi rewrite

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const { i18n } = require("./next-i18next.config");
 module.exports = {
   i18n,
   async rewrites() {
-    return [{ source: "/tr/hakkimizda", destination: "/about", locale: false }];
+    return [
+      { source: "/tr/hakkimizda", destination: "/about", locale: false },
+      { source: "/tr/denemesayfasi", destination: "/testpage", locale: false },
+    ];
   },
   async redirects() {
     return [
